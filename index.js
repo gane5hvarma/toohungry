55ddefe870f5544c2e514155be6b36fe206ac6ee
gane5hvarma <raghavag772@gmail.com>
2018-08-03 02:04:58 +0530

    front end changes added

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ app.use(expressSession({
 }))
 
 
+app.use(function (req, res, next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
 
 //auth route
 const auth = require('./routes/auth'); //auth route
